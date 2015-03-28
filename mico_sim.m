10aba42eb6e6e11cc1ae26e0ae45afdbcf4b5ed2
Maxwell Svetlik <maxsvetlik@utexas.edu>
2015-03-28 13:52:50 -0500

    introducted qlims and inverse kinematics. q and tau fairly accurate compared to actual manipulator

--- mico_sim.m
+++ mico_sim.m
@@ -1,15 +1,15 @@
 function [q_f1 qd_f] = mico_sim(q_t)
-global tsize deg q_0 kp first_run writerdy
+global tsize deg q_0 kp first_run writerdy qdd_lim
 deg = pi/180;
-home = [274.85, 175.147, 78.915, 243.27, 83.9318, 75.136]*deg; %Kinova home position
-writerdy = [262.113, 248.05, 128.54, 343.2272, 72.75, 149.25]*deg;
 tsize = .07;
 first_run = 1;
+qdd_lim = 4;
 q_0 = writerdy; %q naught defines theta at time 0
 kp = .5; %proportionality constant
 mdl_mico;
 mico = evalin('base','mico');
-
+home = evalin('base','home');
+writerdy = evalin('base','writerdy');
 %%%%%%%%%%%%%%%%%
 %q_c = home;
 %t = [0:1:ts];
@@ -24,8 +24,9 @@ q_t1 = writerdy;
 mico.fkine(q_t1)
 %for i = 1:10
 while true
-asdf = [0 0 -.2 0 0 0]';
+asdf = [0 0 -.05 0 0 0]';
 [q_t1, xd_t1] = ts(q_t1, asdf);
+%x = input('Press Enter to continue');
 end
 mico.fkine(q_t1)
 end
@@ -37,23 +38,25 @@ function [q_t1 xd_t1] = ts(q_t, xd_d)
 %         q_t = q_0;
 %         first_run = 0;
 %     end
+    mico.plot(q_t);
     T6 = mico.fkine(q_t);
-    %d6 = tr2jac(T6) * xd_d;
+    d6 = tr2jac(T6) * (tsize*xd_d);
+    temp = transl(d6(1)+T6(1,4),d6(2)+T6(2,4),-d6(3)+T6(3,4));
     qd_t = inv(mico.jacob0(q_t)) * xd_d; %required joint velocities
     qstar = q_t + (tsize * qd_t');
+    qstar = mico.ikinem(temp,q_t,'qlimits','pweight',100);
     qd_t = qd_t';
     %e = qd_t - qd_d; %P controller
-    %[tau,q] = mico.fdyn(1, @controller, q_t, qd_t, qstar, kp, tsize);
-    tau = mico.rne(q_t, qd_t, kp*(qd_t-q_t)) %singe integration,t = 1
+    %[tau,q] = mico.fdyn(2, @controller, q_t, qd_t, qstar, kp, tsize);
+    tau = mico.rne(q_t, qd_t, kp*(qstar-q_t)); %singe integration,t = 1
     tau2 = mico.gravload(q_t);
     q_t1 = qstar;
     q_t = q_t1;
     xd_t1 = xd_d;
-    mico.plot(q_t);
     end
     function tau = controller(~, t, q_t, qd_t, qstar, P, tsize)
           qstar = q_t + (tsize * qd_t);
-          tau = P*(qstar-q_t);
+          tau = P*(qd_t-q_t);
     end
 %   0.187508 -0.782107 +0.285158 -0.0863431 -0.0341825-0.0777281 %real
 %   -1.0266  -11.1628    3.1100   -1.0630   -0.0930    0.0000 %tau
@@ -87,3 +90,7 @@ function [q_t1 xd_t1] = ts(q_t, xd_d)
 %   and then integrate the robot dynamics with the control
 %  
 %          [t,q] = robot.fdyn(10, @mytorqfun, qstar, P, D);
+%         4.6719    2.7965   -4.3915    8.1878   -4.8233
+%             4.6162    5.6587    3.1950    6.1731    0.7063
+
+
